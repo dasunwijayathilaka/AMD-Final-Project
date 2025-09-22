7a02db98e2764bc54f5fa06c167f089989731161
Dasun Wijayathilaka <maxdasun613@gmail.com>
2025-09-22 10:35:10 +0530

    fixed layout problem

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,6 +5,7 @@ const AuthLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen name="register" options={{ title: "Register" }} />
     </Stack>
   );
 };
